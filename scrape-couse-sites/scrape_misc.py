d78ca93a2606e50b9284fb97f0577f42514f051a
ssamba84 <ssamba84@gmail.com>
2019-05-24 20:32:17 -0700

    list of sitemaps-map of site and sitemapurl

--- scrape-couse-sites/scrape_misc.py
+++ scrape-couse-sites/scrape_misc.py
@@ -1,11 +1,13 @@
 from ScrapeSiteMap import SiteMapScrapper
- 
+def printsiteMaps(siteMapMap):
+	for site, sitemap in siteMapMap.items():
+		print(site + " : " + ",".join(sitemap)) 
 def main():
 	sms = SiteMapScrapper()
 	eduSiteFile = open("eduSites.txt")
 	lines = eduSiteFile.readlines()
 	sites = [line.rstrip("\n") for line in lines]
-	siteMapLst =  [sms.getSiteMapUrl(x) for x in sites ]
-	print siteMapLst
+	siteMapMap =  dict([(x, sms.getSiteMapUrl(x)) for x in sites ])
+	printsiteMaps(siteMapMap)
 if __name__ == "__main__":
 	main()
